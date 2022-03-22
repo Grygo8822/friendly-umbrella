1979961a8b985ef9b9d858a7a2df6c566d9c1dd8
Jan Zbyteczny <81484388+Grygo8822@users.noreply.github.com>
2022-03-22 22:19:29 +0100

    Add files via upload

--- k-means-vis.py
+++ k-means-vis.py
@@ -7,96 +7,97 @@ from random import random
 import this
 import tkinter
 import csv
+from turtle import distance
 from typing import Iterator
 from PIL import ImageTk, Image
 import numpy as np
 import matplotlib.pyplot as plt
 import random  
 import math
+import os
+
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+filename = os.path.join(here, 'data.txt')
 
 petal_points =[]
 k = 2
-
-class point:
-    
-    def __init__(self,x_value, y_value):
-        self.x_value = x_value
-        self.y_value = y_value
-    
-    def print(self):
-        print(self.x_value, self.y_value)
-
-def calculate_distance(p1, p2):
-    return (sqrt(float(pow((p2.x_value - p1.x_value),2) + pow((p2.y_value - p1.y_value),2))))
         
 
 def generate_data_points(filename,delimiter ="\t"):
-    with open(filename) as petal_data:
+
+    with open(filename) as data:
         output = []
-        petal_data_reader = csv.reader(petal_data,delimiter=delimiter)
-        #Skip the first entry: Column names
-        iter_data = iter(petal_data_reader)
-        next(iter_data)
-        for row in iter_data:
-            x_value = float(row[0])
-            y_value = float(row[1])
-            output.append(point(x_value,y_value))
+        data_reader = csv.reader(data,delimiter=delimiter)
+        for row in data_reader:
+            output.append( (float(row[0].replace(",",".")), float(row[1].replace(",",".")), float(row[2].replace(",",".")),float(row[3].replace(",","."))))
             
     return output
 
+def select_random_points(points, number_of_points_to_pick = 3):
+    output = []
+    for x in range(0,k):
+        output.append(random.choice(points))
+    return output
+
+# calculate distance and group each point
+def cluster_points(points, centroids):
+    output = {}
+    for x in range(k):
+        output[x] = []
+    for p in petal_points:
+        shortest_distance = math.inf
+        group_to_assign = 0
+        for index, c in enumerate(centroids):
+            distance = math.dist(c, p)
+            if distance < shortest_distance:
+                shortest_distance = distance
+                group_to_assign = index
+        output[group_to_assign].append(p)
+    return output
+
+def calculate_new_centroids(centroids, point_groupings):
+    for index,c in enumerate(centroids):
+        for cluster in range(len(point_groupings)):
+            averages = [0,0,0,0]
+            for p in point_groupings[cluster]:
+               averages[0] += p[0]
+               averages[1] += p[1]
+               averages[2] += p[2]
+               averages[3] += p[3]
+
+            points_in_cluster = len(point_groupings[cluster])
+            if points_in_cluster != 0:
+               
+               averages[0] = averages[0] / points_in_cluster
+               averages[1] = averages[1] / points_in_cluster
+               averages[2] = averages[2] / points_in_cluster
+               averages[3] = averages[3] / points_in_cluster
+            else:
+                averages = [0,0,0,0]
+            c = averages
 
 #sepal_points = generate_data_points("sepal_data.txt")
 
-petal_points = generate_data_points("petal_data.txt")
+petal_points = generate_data_points(filename)
 
-#initialize the output dictionary
-point_groupings = {}
+centroids = select_random_points(petal_points, k)
 
-for x in range(0,k):
-    point_groupings[x]= []
+clustered_points = cluster_points(petal_points, centroids)
 
-#select k random points
-centroids = []
+calculate_new_centroids(centroids, clustered_points)
 
-for x in range(0,k):
-    new_centroid = centroids.append(random.choice(petal_points))
+for item in clustered_points.items():
+    plt.plot(item)
 
-# calculate distance and group each point
-for p in petal_points:
-    shortest_distance = math.inf
-    group_to_assign = 0
-    for index, c in enumerate(centroids):
-        distance = math.dist([p.x_value, p.y_value], [c.x_value, c.y_value])
-        if distance < shortest_distance:
-            shortest_distance = distance
-            group_to_assign = index
-    point_groupings[group_to_assign].append(p)
-
-for c in centroids:
-    c.print()
-print("***********")
-
-#calculate new centroids
-for index,c in enumerate(centroids):
-    for cluster in range(len(point_groupings)):
-        avg_x =0
-        avg_y =0
-        for p in point_groupings[cluster]:
-            avg_x += p.x_value
-            avg_y += p.y_value
-        if len(point_groupings[cluster]) != 0:
-            avg_x = avg_x / len(point_groupings[cluster])
-            avg_y = avg_y / len(point_groupings[cluster])
-        else:
-            avg_x = 0
-            avg_y = 0
-        c.x_value = avg_x
-        c.y_value = avg_y
-
-for c in centroids:
-    c.print()
+plt.show()
+clustered_points = cluster_points(petal_points, centroids)
 
 print()
+
+
 
 
 
